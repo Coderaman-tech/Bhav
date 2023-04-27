9a27b87c9b835666551738a163982cfb2bacdcee
coderaman_tech <amankumarshrivastava835@gmail.com>
2023-04-27 23:58:53 +0530

    initial commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Intro.css';
+import Lottie from "lottie-react";
+import Post from '../../media/animations/social-media-influencer.json';
+import twitter from '../../media/images/twitter.png';
+import instagram from '../../media/images/instagram.png';
+import linkedin from '../../media/images/linkedin.png';
+import fb from '../../media/images/fb.png';
+
+function Intro() {
+  return (
+    <>
+    <div className='intro'>
+        
+        <div className="i-left">
+            <div className="i-name">
+                <span>Bhav</span>
+                <span>
+                Welcome to Bhav - your ultimate social media content assistant. Our cutting-edge sentiment analysis technology provides personalized content recommendations for Twitter, Facebook, LinkedIn, and other platforms. Whether you're a social media influencer, marketer, or business owner, Bhav helps you create engaging content that resonates with your audience.
+                </span>
+            </div>
+
+            <button className='button i-button'>
+                Try it
+            </button>
+        </div>
+
+        <div className="i-right">
+        <Lottie style={{height:600}} animationData={Post} />
+        </div>
+    </div>
+     <div className="term1">Help for platform</div>
+     
+     <div className="use-case">
+     <img src={twitter} alt="" width={87} height={90}/>
+     <img src={linkedin} alt="" width={87} height={90}/>
+     <img src={fb} alt="" width={87} height={90}/>
+     <img src={instagram} alt="" width={87} height={90}/>
+ </div>
+ </>
+  )
+}
+
+export default Intro
