ff5d3f88ee8350cff04567f746ee03ce322085db
Aman Shrivastava <amankumarshrivastava835@gmail.com>
2026-06-21 13:28:33 +0530

    add login-logout-api

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const authRouter=express.Router();
 const User=require("../models/user");
+const bcrypt=require("bcrypt");
 const {validatesSignUpData}=require("../utils/validation");
 
 authRouter.post("/signup",async(req,res)=>{
@@ -20,5 +21,32 @@ authRouter.post("/signup",async(req,res)=>{
     }
 });
 
+authRouter.post("/login",async(req,res)=>{
+    try{
+        const {emailId,password}=req.body;
+        const user=await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            const token=await user.getJWT();
+            res.cookie("token",token,{expires:new Date(Date.now()+8*3600000)});
+            res.send("Login successfull!!!");
+        }
+        else{
+            throw new Error("Invalid Credentials");
+        }
+    }catch(err){
+        res.status(500).send("Error : "+err.message);
+    }
+});
+
+authRouter.post("/logout",async(req,res)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+    });
+    res.send("Logout Successful!!!");
+});
 
 module.exports=authRouter;
